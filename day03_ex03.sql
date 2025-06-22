1588b6b93470c208b7c6f35832a30fe4ef8c54be
vrtchkhihihih <1ruhxf1@gmail.com>
2025-06-22 13:31:17 +0300

    day03_ex03.sql

--- day03_ex03.sql
+++ day03_ex03.sql
@@ -1,14 +1,17 @@
 WITH pvp AS (
-    SELECT pizzeria.name, gender FROM person_visits
-        JOIN person ON person_id = person.id
-        JOIN pizzeria ON pizzeria_id = pizzeria.id),
-    pv_male AS (SELECT name FROM pvp WHERE gender = 'male'),
-    pv_female AS (SELECT name FROM pvp WHERE gender = 'female')
-
+    SELECT p.name, per.gender
+    FROM person_visits pv
+    JOIN person per ON pv.person_id = per.id
+    JOIN pizzeria p ON pv.pizzeria_id = p.id
+),
+     pv_male AS (SELECT name FROM pvp WHERE gender = 'male'),
+     pv_female AS (SELECT name FROM pvp WHERE gender = 'female')
 SELECT name AS pizzeria_name
 FROM (
-    (SELECT * FROM pv_male EXCEPT ALL SELECT * FROM pv_female)
-    UNION ALL
-    (SELECT * FROM pv_female EXCEPT ALL SELECT * FROM pv_male)
-     ) AS uall
+         SELECT name FROM pv_male EXCEPT ALL SELECT name FROM pv_female
+     )
+UNION ALL
+(
+    SELECT name FROM pv_female EXCEPT ALL SELECT name FROM pv_male
+)
 ORDER BY pizzeria_name;
